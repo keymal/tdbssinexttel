deec92b204d6241fbece7abe65f6fdfd5246df4c
André AMOUGOU BELLA <amougouandre37@gmail.com>
2022-10-06 17:37:56 +0100

    Installation des dépendances

---
+++ src/main/java/com/example/tdbssinexttel/TdbssinexttelApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdbssinexttel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdbssinexttelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdbssinexttelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tdbssinexttel/TdbssinexttelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdbssinexttel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdbssinexttelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
